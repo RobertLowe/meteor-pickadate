70903c0019329a3ff4eb3695475d705b9b371bc6
Kevin Green <kevin@swedenunlimited.com>
2014-11-06 17:35:26 -0500

    updated version

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('0.9.0');
+  api.versionsFrom('1.0.0');
   api.use('jquery', 'client');
 
   api.addFiles('pickadate.js/lib/picker.js', 'client');
