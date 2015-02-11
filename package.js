dbc4d222fe30ec8aa3d6782d3dd5b7e38ac45dba
Robert Lowe <robert@iblargz.com>
2015-02-11 17:16:24 -0500

    Ensure raw, and switch to patched version

--- package.js
+++ package.js
@@ -9,10 +9,10 @@ Package.onUse(function (api) {
   api.versionsFrom('1.0');
   api.use('jquery', 'client');
 
-  api.addFiles('pickadate.js/lib/picker.js', 'client');
-  api.addFiles('pickadate.js/lib/picker.date.js', 'client');
-  api.addFiles('pickadate.js/lib/picker.time.js', 'client');
-  api.addFiles('pickadate.js/lib/legacy.js', 'client');
+  api.addFiles('pickadate.js/lib/picker.js', 'client', {raw: true});
+  api.addFiles('pickadate.js/lib/picker.date.js', 'client', {raw: true});
+  api.addFiles('pickadate.js/lib/picker.time.js', 'client', {raw: true});
+  api.addFiles('pickadate.js/lib/legacy.js', 'client', {raw: true});
   api.addFiles('pickadate.js/lib/themes/default.css', 'client');
   api.addFiles('pickadate.js/lib/themes/default.date.css', 'client');
   api.addFiles('pickadate.js/lib/themes/default.time.css', 'client');
